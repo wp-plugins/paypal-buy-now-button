1c324ad2d765fb74f62b145e4e7cfa243496feb7
johnny manziel <johnny manziel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-12 03:46:11 +0000

    initial commit
    git-svn-id: https://plugins.svn.wordpress.org/paypal-buy-now-button/trunk@1110823 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-paypal-buy-now-button-deactivator.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @class       MBJ_PayPal_Buy_Now_Button_Deactivator
+ * @version	1.0.0
+ * @package	paypal-buy-now-button
+ * @category	Class
+ * @author      johnny manziel <phpwebcreators@gmail.com>
+ */
+
+class MBJ_PayPal_Buy_Now_Button_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
