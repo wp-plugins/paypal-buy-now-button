c77ddecaf2558d9fdb24d862ccdc6ad8b6d3455c
johnny manziel <johnny manziel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-12 03:47:19 +0000

    initial tags/1.0.0 commit
    git-svn-id: https://plugins.svn.wordpress.org/paypal-buy-now-button/tags/1.0.0@1110825 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-paypal-buy-now-button-activator.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @class       MBJ_PayPal_Buy_Now_Button_Activator
+ * @version	1.0.0
+ * @package	paypal-buy-now-button
+ * @category	Class
+ * @author      johnny manziel <phpwebcreators@gmail.com>
+ */
+class MBJ_PayPal_Buy_Now_Button_Activator {
+
+    /**
+     *
+     * @since    1.0.0
+     */
+    public static function activate() {
+        
+    }
+
+}
