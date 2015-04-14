06ba8e91528361b89a1f4f78b928151ed1396571
johnny manziel <johnny manziel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-14 19:25:30 +0000

    add new verson
    git-svn-id: https://plugins.svn.wordpress.org/paypal-buy-now-button/trunk@1134792 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- paypal-buy-now-button.php
+++ paypal-buy-now-button.php
@@ -4,10 +4,10 @@
  *
  * @wordpress-plugin
  * Plugin Name:       PayPal Buy Now Button
- * Plugin URI:        http://www.mbjtechnolabs.com
+ * Plugin URI:        http://webs-spider.com/
  * Description:       Easy to use add a PayPal Buy Now Button as a Page, Post and Widget with a shortcode
- * Version:           1.0.5
- * Author:            mbjtechnolabs
+ * Version:           1.0.6
+ * Author:            johnwickjigo
  * Author URI:        http://www.mbjtechnolabs.com
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
